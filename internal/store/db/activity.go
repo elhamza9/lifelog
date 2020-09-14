8b6f4ae00819807086d873b4f0be2aed65029a69
Hamza El Bouatmani <hamza.elbouatmani@gmail.com>
2020-09-14 10:51:59 +0100

    feat(store): Implement FindActivitiesByTime in Db Store

--- internal/store/db/activity.go
+++ internal/store/db/activity.go
@@ -22,14 +22,34 @@ func (repo Repository) FindActivityByID(id domain.ActivityID) (domain.Activity, 
 
 // SaveActivity stores the given activity in memory and returns created activity
 func (repo Repository) SaveActivity(act domain.Activity) (domain.ActivityID, error) {
-	return 0, errNotImplemented
+	tags := []Tag{}
+	for _, t := range act.Tags {
+		tags = append(tags, Tag{ID: t.ID, Name: t.Name})
+	}
+	dbAct := Activity{
+		ID:    act.ID,
+		Label: act.Label,
+		Place: act.Place,
+		Desc:  act.Desc,
+		Time:  act.Time,
+		Tags:  tags,
+	}
+	res := repo.db.Create(&dbAct)
+	return domain.ActivityID(dbAct.ID), res.Error
 }
 
 // FindActivitiesByTime returns activities
 // with Time field greater than or equal to the given time
 func (repo Repository) FindActivitiesByTime(t time.Time) ([]domain.Activity, error) {
-	res := []domain.Activity{}
-	return res, errNotImplemented
+	res := []Activity{}
+	if err := repo.db.Where("time >= ?", t).Order("time DESC").Find(&res).Error; err != nil {
+		return []domain.Activity{}, err
+	}
+	activities := make([]domain.Activity, len(res))
+	for i, exp := range res {
+		activities[i] = exp.ToDomain()
+	}
+	return activities, nil
 }
 
 // FindActivitiesByTag returns actenses that have the provided tag in their Tags field
--- internal/store/db/activity_test.go
+++ internal/store/db/activity_test.go
@@ -1,6 +1,8 @@
 package db_test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
@@ -42,11 +44,77 @@ func TestFindActivityByID(t *testing.T) {
 }
 
 func TestSaveActivity(t *testing.T) {
-	t.Fatal("Test not yet implemented")
+	// Create test Activity
+	defer clearDB()
+	tags := []db.Tag{{ID: 1, Name: "test-tag-1"}, {ID: 2, Name: "test-tag-2"}}
+	if err := grmDb.Create(&tags).Error; err != nil {
+		t.Fatalf("\nError while creating Test Tags:\n  %v", err)
+	}
+	act := domain.Activity{
+		ID:       546,
+		Label:    "test activity",
+		Place:    "Somewhere",
+		Desc:     "Details",
+		Time:     time.Now(),
+		Duration: time.Duration(time.Hour),
+		Tags:     []domain.Tag{{ID: tags[0].ID}},
+	}
+	// Test Save
+	id, err := repo.SaveActivity(act)
+	if err != nil {
+		t.Fatalf("\nUnexpected Error: %v", err)
+	}
+	var created db.Activity
+	if err := grmDb.Preload("Tags").First(&created, id).Error; err != nil {
+		t.Fatalf("\nUnexpected Error while retrieving saved activity:\n  %v", err)
+	}
+	if len(created.Tags) != len(act.Tags) {
+		t.Fatalf("\nExpected %d Tags\nReturned %d Tags", len(act.Tags), len(created.Tags))
+	}
 }
 
 func TestFindActivitiesByTime(t *testing.T) {
-	t.Fatal("Test not yet implemented")
+	// Create test 100 activities:
+	// one for each day starting from today going backward
+	defer clearDB()
+	const nbrActivities int = 100
+	activities := make([]db.Activity, nbrActivities)
+	now := time.Now()
+	for i := 0; i < nbrActivities; i++ {
+		activities[i] = db.Activity{
+			Label:    fmt.Sprintf("Test Activity %d", i),
+			Place:    "Somewhere",
+			Desc:     "Details",
+			Time:     now.AddDate(0, 0, -i),
+			Duration: time.Duration(time.Hour),
+			Tags:     []db.Tag{},
+		}
+	}
+	// Shuffle activities before saving them to DB to avoid getting them by insertion order
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(activities), func(i, j int) { activities[i], activities[j] = activities[j], activities[i] })
+	if err := grmDb.Create(&activities).Error; err != nil {
+		t.Fatalf("\nError while creating test activities:\n  %v", err)
+	}
+	// Test Get Activities of last 5 days (Should be 6 activities)
+	minTime := now.AddDate(0, 0, -5)
+	nbrExpectedActivities := 6
+	res, err := repo.FindActivitiesByTime(minTime)
+	if err != nil {
+		t.Fatalf("\nUnexpected Error: %v", err)
+	}
+	if len(res) != nbrExpectedActivities {
+		t.Log(res)
+		t.Fatalf("\nExpecting %d Activities\nReturned %d activities", nbrExpectedActivities, len(res))
+	}
+	// Test Order by time
+	for i, exp := range res {
+		if i < len(res)-1 {
+			if exp.Time.Before(res[i+1].Time) {
+				t.Fatal("Activities not ordered by time")
+			}
+		}
+	}
 }
 
 func TestFindActivitiesByTag(t *testing.T) {
