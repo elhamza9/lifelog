83bf79f0829da4dbca0cd83403d7df51b54e0e50
Hamza El Bouatmani <hamza.elbouatmani@gmail.com>
2020-09-19 11:43:59 +0100

    feat(rest/client): Add RefreshToken

--- internal/http/rest/client/auth.go
+++ internal/http/rest/client/auth.go
@@ -23,6 +23,17 @@ type loginRespPayload struct {
 	Refresh string `json:"rt"`
 }
 
+// refreshReqPayload is used to marshall credentials to JSON when refreshing token
+type refreshReqPayload struct {
+	Refresh string `json:"refresh"`
+}
+
+// refreshRespPayload is used to unmarshall JSON Response
+// when refreshing token is successful
+type refreshRespPayload struct {
+	Refresh string `json:"rt"`
+}
+
 // Login sends a POST request with password and
 // gets a Jwt Token Pair if password is correct
 func Login(pass string) (string, string, error) {
@@ -55,4 +66,38 @@ func Login(pass string) (string, string, error) {
 		return "", "", err
 	}
 	return respObj.Access, respObj.Refresh, nil
+}
+
+// RefreshToken sends a POST request with refresh token and
+// gets a new Jwt Access Token
+func RefreshToken(token string) (string, error) {
+	// Marshall Tag to JSON
+	payload := refreshReqPayload{Refresh: token}
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+	// Send HTTP Request
+	const path string = url + "/auth/refresh"
+	requestBody := bytes.NewBuffer(jsonPayload)
+	resp, err := http.Post(path, "application/json", requestBody)
+	if err != nil {
+		return "", err
+	}
+	// Read Response
+	responseCode := resp.StatusCode
+	responseBody, err := readResponseBody(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	// Check Response Code
+	if responseCode != http.StatusOK {
+		return "", fmt.Errorf("error refreshing token:\n\t- code: %d\n\t- body: %s\n", responseCode, responseBody)
+	}
+	// Extract Tokens
+	respObj := refreshRespPayload{}
+	if err := json.Unmarshal(responseBody, &respObj); err != nil {
+		return "", err
+	}
+	return respObj.Refresh, nil
 }
--- internal/http/rest/client/auth_test.go
+++ internal/http/rest/client/auth_test.go
@@ -12,3 +12,11 @@ func TestLogin(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestRefreshToken(t *testing.T) {
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.G2f51qduWeW9XHILKdP-ZROENssiPEUG_I_pOB-3298"
+	_, err := client.RefreshToken(token)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
