7a04a19ad03433f1479e7ad7cffef5a3dd2e7f20
Hamza El Bouatmani <hamza.elbouatmani@gmail.com>
2020-09-22 07:33:25 +0100

    fix(cli): Fix Spaces in Select Tag Template

--- internal/cli/tag_io.go
+++ internal/cli/tag_io.go
@@ -2,7 +2,11 @@ package cli
 
 import (
 	"errors"
+	"fmt"
+	"log"
+	"strconv"
 	"strings"
+	"text/template"
 
 	"github.com/elhamza90/lifelog/internal/domain"
 	"github.com/manifoldco/promptui"
@@ -39,24 +43,45 @@ func tagNameValidator(input string) error {
 
 // tagSelect list given tags and asks user to select one.
 func tagSelect(tags []domain.Tag) (selectedTagIndex int, err error) {
-	/*
-		var idMaxLen int = 0
-		for _, t := range tags {
-			idStr := strconv.Itoa(int(t.ID))
-			if len(idStr) > idMaxLen {
-				idMaxLen = len(idStr)
-			}
+	var idMaxLen int = 0
+	for _, t := range tags {
+		idStr := strconv.Itoa(int(t.ID))
+		if len(idStr) > idMaxLen {
+			idMaxLen = len(idStr)
 		}
-	*/
+	}
+	log.Println(idMaxLen)
 	templates := &promptui.SelectTemplates{
 		Label:    "{{ . }}",
-		Inactive: "\t - " + `{{ printf "[%d] " .ID | white }}` + `| {{ .Name | white }}`,
-		Active: "\t → " +
-			`{{ printf "[%d] " .ID | cyan | bold }}` +
-			`| {{ .Name | cyan | bold }}`,
-		Selected: "\t → " +
-			`{{ printf "[%d] " .ID | green | bold }}` +
-			`| {{ .Name | green | bold }}`,
+		Inactive: "\t - " + `{{ printf "[%d] " .ID}} ` + fmt.Sprintf("{{ fixSpaces .ID %d}}", idMaxLen) + ` | {{ .Name | white }}`,
+		Active:   "\t → " + `{{ printf "[%d]  " .ID|cyan|bold}} ` + fmt.Sprintf("{{ fixSpaces .ID %d}}", idMaxLen) + ` | {{ .Name | cyan | bold }}`,
+		Selected: "\t → " + `{{ printf "[%d] " .ID| green | bold }}` + ` | {{ .Name | green | bold }}`,
+		FuncMap: template.FuncMap{
+			"fixSpaces": func(id domain.TagID, maxLen int) string {
+				times := maxLen - len(strconv.Itoa(int(id)))
+				return strings.Repeat(" ", times)
+			},
+			"black":     promptui.Styler(promptui.FGBlack),
+			"red":       promptui.Styler(promptui.FGRed),
+			"green":     promptui.Styler(promptui.FGGreen),
+			"yellow":    promptui.Styler(promptui.FGYellow),
+			"blue":      promptui.Styler(promptui.FGBlue),
+			"magenta":   promptui.Styler(promptui.FGMagenta),
+			"cyan":      promptui.Styler(promptui.FGCyan),
+			"white":     promptui.Styler(promptui.FGWhite),
+			"bgBlack":   promptui.Styler(promptui.BGBlack),
+			"bgRed":     promptui.Styler(promptui.BGRed),
+			"bgGreen":   promptui.Styler(promptui.BGGreen),
+			"bgYellow":  promptui.Styler(promptui.BGYellow),
+			"bgBlue":    promptui.Styler(promptui.BGBlue),
+			"bgMagenta": promptui.Styler(promptui.BGMagenta),
+			"bgCyan":    promptui.Styler(promptui.BGCyan),
+			"bgWhite":   promptui.Styler(promptui.BGWhite),
+			"bold":      promptui.Styler(promptui.FGBold),
+			"faint":     promptui.Styler(promptui.FGFaint),
+			"italic":    promptui.Styler(promptui.FGItalic),
+			"underline": promptui.Styler(promptui.FGUnderline),
+		},
 	}
 	tagPrompt := promptui.Select{
 		Label:     "Choose Tag:",
