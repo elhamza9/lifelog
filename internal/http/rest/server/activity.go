de5e59e1a1b16239b159a77d69f34857e9712cad
Hamza El Bouatmani <hamza.elbouatmani@gmail.com>
2020-09-29 20:14:17 +0100

    feat(rest/server): Add logs to activity handlers

--- internal/http/rest/server/activity.go
+++ internal/http/rest/server/activity.go
@@ -1,12 +1,14 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/elhamza90/lifelog/internal/domain"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 )
 
 // defaultActivitiesMinDate specifies default date filter when listing activities
@@ -21,6 +23,7 @@ func defaultActivitiesDateFilter() time.Time {
 // If no parameter is found default is to return activities of last 3 months
 func (h *Handler) ActivitiesByDate(c echo.Context) error {
 	dateStr := c.QueryParam("from")
+	logrus.Debugf("Extracted query param from: %s", dateStr)
 	var date time.Time
 	if len(dateStr) == 0 {
 		date = defaultActivitiesDateFilter()
@@ -28,14 +31,20 @@ func (h *Handler) ActivitiesByDate(c echo.Context) error {
 		var err error
 		date, err = time.Parse(dateFilterFormat, dateStr)
 		if err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
+			msg := "Error parsing from param to valid date"
+			details := err.Error()
+			logrus.Error(msg + ": " + details)
+			return c.String(http.StatusBadRequest, msg)
 		}
 	}
 	// Fetch Activities
 	activities, err := h.lister.ActivitiesByTime(date)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while fetching activities since %s", date.Format("2006-01-02"))
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Fetched activities since %s successfully", date.Format("2006-01-02"))
 	// Construct respActivities from fetched activities
 	respActivities := make([]JSONRespListActivity, len(activities))
 	var respAct JSONRespListActivity
@@ -53,18 +62,28 @@ func (h *Handler) ActivityDetails(c echo.Context) error {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		msg := fmt.Sprintf("Error while converting path param Activity ID with value %s to int", idStr)
+		details := err.Error()
+		logrus.Error(msg + " | " + details)
+		return c.String(http.StatusBadRequest, msg)
 	}
+	actID := domain.ActivityID(id)
 	// Get Activity
-	act, err := h.lister.Activity(domain.ActivityID(id))
+	act, err := h.lister.Activity(actID)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while fetching activity %s", actID)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Fetched activity %s successfully", actID)
 	// Fetch Expenses
 	expenses, err := h.lister.ExpensesByActivity(act.ID)
 	if err != nil {
+		msg := fmt.Sprintf("Internal Server Error while fetching expenses of activity %s", actID)
+		logrus.Error(msg + " : " + err.Error())
 		return c.String(errToHTTPCode(err, "activities"), err.Error())
 	}
+	logrus.Infof("Fetched expenses of activity %s successfully", actID)
 	var actResp JSONRespDetailActivity
 	actResp.From(act, expenses)
 	return c.JSON(http.StatusOK, actResp)
@@ -75,17 +94,28 @@ func (h *Handler) AddActivity(c echo.Context) error {
 	// Json unmarshall
 	var jsAct JSONReqActivity
 	if err := c.Bind(&jsAct); err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		var (
+			msg     string = errInvalidJSON.Error()
+			details string = httpErrorMsg(err)
+			code    int    = errToHTTPCode(errInvalidJSON, "activities")
+		)
+		logrus.Error(msg + " : " + details)
+		return c.String(code, msg)
 	}
 	act := jsAct.ToDomain()
 	id, err := h.adder.NewActivity(act)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := "Internal Server Error while adding activity"
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Created activity %s successfully", id)
 	// Retrieve created activity
 	created, err := h.lister.Activity(id)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while fetching activity %s", id)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
 	return c.JSON(http.StatusCreated, created)
 }
@@ -97,28 +127,47 @@ func (h *Handler) EditActivity(c echo.Context) error {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		msg := fmt.Sprintf("Error while converting path param Activity ID with value %s to int", idStr)
+		details := err.Error()
+		logrus.Error(msg + " | " + details)
+		return c.String(http.StatusBadRequest, msg)
 	}
+	actID := domain.ActivityID(id)
 	// Get Activity
-	act, err := h.lister.Activity(domain.ActivityID(id))
+	act, err := h.lister.Activity(actID)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while fetching activity %s", actID)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Fetched activity %s successfully", actID)
 	// Json unmarshall
 	var jsAct JSONReqActivity
 	if err := c.Bind(&jsAct); err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		var (
+			msg     string = errInvalidJSON.Error()
+			details string = httpErrorMsg(err)
+			code    int    = errToHTTPCode(errInvalidJSON, "activities")
+		)
+		logrus.Error(msg + " | " + details)
+		return c.String(code, msg)
 	}
 	updated := jsAct.ToDomain()
 	err = h.editor.EditActivity(updated)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while updating activity %s", actID)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Updated activity %s successfully", actID)
 	// Retrieve edited activity
 	edited, err := h.lister.Activity(act.ID)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while fetching updated activity %s", act.ID)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Fetched activity %s successfully", act.ID)
 	return c.JSON(http.StatusOK, edited)
 }
 
@@ -129,12 +178,19 @@ func (h *Handler) DeleteActivity(c echo.Context) error {
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		msg := fmt.Sprintf("Error while converting path param Activity ID with value %s to int", idStr)
+		details := err.Error()
+		logrus.Error(msg + " | " + details)
+		return c.String(http.StatusBadRequest, msg)
 	}
+	actID := domain.ActivityID(id)
 	// Delete Activity
-	err = h.deleter.Activity(domain.ActivityID(id))
+	err = h.deleter.Activity(actID)
 	if err != nil {
-		return c.String(errToHTTPCode(err, "activities"), err.Error())
+		msg := fmt.Sprintf("Internal Server Error while deleting activity %s", actID)
+		logrus.Error(msg + " : " + err.Error())
+		return c.String(errToHTTPCode(err, "activities"), msg)
 	}
+	logrus.Infof("Deleted activity %s successfully", actID)
 	return c.JSON(http.StatusNoContent, "Activity Deleted Successfully")
 }
